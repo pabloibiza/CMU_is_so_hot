0aab69e5e4b3aa777cc47fc01a7da87feb82c892
Pablo <pau.ibiza@hotmail.com>
2021-04-16 09:50:49 +0200

    Projects files added to modules

---
+++ Raspberry/CMU_is_so_hot-Raspberry/src/main/java/com/mycompany/cmu_is_so_hot/raspberry/JAXRSConfiguration.java
@@ -0,0 +1,13 @@
+package com.mycompany.cmu_is_so_hot.raspberry;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures JAX-RS for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+    
+}
