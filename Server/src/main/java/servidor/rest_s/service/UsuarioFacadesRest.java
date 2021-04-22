bc3379cf6df232ddad505a7846ce66efacc7b523
Carlos Castillo Deza <carloscastillodeza@gmail.com>
2021-04-22 12:06:56 +0200

    Creación Usuario, UsuarioFacades, Persistence,
    Creación de las distintas clases e interfaz. Cambio también del Persistence

---
+++ Server/src/main/java/servidor/rest_s/service/UsuarioFacadesRest.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servidor.rest_s.service;
+
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import servidor.rest_s.Usuario;
+
+/**
+ *
+ * @author karlo
+ */
+
+@Stateless
+@Path("servidor_rest_s.usuario")
+public class UsuarioFacadesRest extends UniversalFacade<Usuario> {
+    
+    @PersistenceContext(unitName = "com.mycompany_CMU_is_so_hotPU")
+    private EntityManager entity; 
+
+    public UsuarioFacadesRest() {
+        super(Usuario.class);
+    }
+
+    @POST
+    @Override
+    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public void create(Usuario entity) {
+        super.create(entity);
+    }
+
+    @PUT
+    @Path("{id}")
+    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public void edit(@PathParam("id") Long id, Usuario entity) {
+        super.edit(entity);
+    }
+
+    @DELETE
+    @Path("{id}")
+    public void remove(@PathParam("id") Long id) {
+        super.remove(super.find(id));
+    }
+
+    @GET
+    @Path("{id}")
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public Usuario find(@PathParam("id") Long id) {
+        return super.find(id);
+    }
+
+    @GET
+    @Override
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public List<Usuario> findAll() {
+        return super.findAll();
+    }
+
+    @GET
+    @Path("{from}/{to}")
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public List<Usuario> findRange(@PathParam("from") Integer from, @PathParam("to") Integer to) {
+        return super.findRange(new int[]{from, to});
+    }
+
+    @GET
+    @Path("count")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String countREST() {
+        return String.valueOf(super.count());
+    }
+    
+    @Override
+    protected EntityManager getEntityManager() {
+        return entity;
+    }
+    
+}
